007861fce700b3d00ec3c8a532dceaccc64f8510
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2017-01-31 22:25:52 +0100

    [examples][common_die] add color log at examples
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

--- examples/common_die/main.c
+++ examples/common_die/main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "log.h"
 
@@ -11,8 +12,18 @@ static int _test_common_die_zero( int value){
 int main( void){
 	int ret;
 	printf( "Start Test common_die\n");
+	ret = libcommon_init_log();
 
+	printf("Log without color by default\n");
 	ret = _test_common_die_zero(-1);
 	printf( "Test common die zero return: %d\n", ret);
+
+	printf("Set log with color\n");
+	ret = libcommon_set_color_log( true);
+
+	printf( "Try color log\n");
+	ret = _test_common_die_zero(-1);
+	printf( "Test common die zero return: %d\n", ret);
+
 	return 0;
 }
