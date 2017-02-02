8b5d383446bca5c2ac4d8e2454e56217f5856243
LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>
2017-02-02 22:14:28 +0100

    [build] improve cross-compil by adding arch in toolchain conf file
    Signed-off-by: LAMBS Pierre-Antoine <pierreantoine.lambs@gmail.com>

--- examples/common_die/make_target.sh
+++ examples/common_die/make_target.sh
@@ -9,18 +9,19 @@ if [ ! -e ${config_file} ]; then
 	cp ${defconfig_file} ${config_file}
 fi
 
-toolchain_path=$(grep "ARM_TOOLCHAIN_PATH" ${config_file} | awk -F'\"' '{print $2}')
-toolchain_prefix=$(grep "ARM_TOOLCHAIN_PRFIX" ${config_file} | awk -F'\"' '{print $2}')
+toolchain_path=$(grep "TOOLCHAIN_PATH" ${config_file} | awk -F'\"' '{print $2}')
+toolchain_prefix=$(grep "TOOLCHAIN_PRFIX" ${config_file} | awk -F'\"' '{print $2}')
 sysroot=$(grep "SYSROOT" ${config_file} | awk -F'\"' '{print $2}')
+arch=$(grep "ARCH" ${config_file} | awk -F'\"' '{print $2}')
 
 #Relocate toolchain folder
 toolchain_path="../../${toolchain_path}"
 sysroot="../../${sysroot}"
 
-export ARM_TOOLCHAIN_PATH=${toolchain_path}
-export ARM_TOOLCHAIN_PRFIX=${toolchain_prefix}
+export TOOLCHAIN_PATH=${toolchain_path}
+export TOOLCHAIN_PRFIX=${toolchain_prefix}
 
-export LD_LIBRARY_PATH=$ARM_TOOLCHAIN_PATH/usr/lib
-export PATH=$ARM_TOOLCHAIN_PATH/usr/bin:$PATH
+export LD_LIBRARY_PATH=$TOOLCHAIN_PATH/usr/lib
+export PATH=$TOOLCHAIN_PATH/usr/bin:$PATH
 
-make --no-print-directory  ARCH=arm CROSS_COMPILE=$ARM_TOOLCHAIN_PRFIX SYSROOT=${sysroot} $1
+make ARCH=$arch CROSS_COMPILE=$TOOLCHAIN_PRFIX SYSROOT=${sysroot} $1
--- make_target.sh
+++ make_target.sh
@@ -9,19 +9,15 @@ if [ ! -e ${config_file} ]; then
 	cp ${defconfig_file} ${config_file}
 fi
 
-toolchain_path=$(grep "ARM_TOOLCHAIN_PATH" ${config_file} | awk -F'\"' '{print $2}')
-toolchain_prefix=$(grep "ARM_TOOLCHAIN_PRFIX" ${config_file} | awk -F'\"' '{print $2}')
+toolchain_path=$(grep "TOOLCHAIN_PATH" ${config_file} | awk -F'\"' '{print $2}')
+toolchain_prefix=$(grep "TOOLCHAIN_PRFIX" ${config_file} | awk -F'\"' '{print $2}')
 sysroot=$(grep "SYSROOT" ${config_file} | awk -F'\"' '{print $2}')
+arch=$(grep "ARCH" ${config_file} | awk -F'\"' '{print $2}')
 
-#printf "toolchain_path  :  %s\n" "${toolchain_path}"
-#printf "toolchain_prefix:  %s\n" "${toolchain_prefix}"
-#printf "sysroot         :  %s\n" "${sysroot}"
+export TOOLCHAIN_PATH=${toolchain_path}
+export TOOLCHAIN_PRFIX=${toolchain_prefix}
 
-export ARM_TOOLCHAIN_PATH=${toolchain_path}
-export ARM_TOOLCHAIN_PRFIX=${toolchain_prefix}
+export LD_LIBRARY_PATH=$TOOLCHAIN_PATH/usr/lib
+export PATH=$TOOLCHAIN_PATH/usr/bin:$PATH
 
-export LD_LIBRARY_PATH=$ARM_TOOLCHAIN_PATH/usr/lib
-export PATH=$ARM_TOOLCHAIN_PATH/usr/bin:$PATH
-
-if [ $# -eq 0 ] ; then echo ; echo " ---==== $(basename $(pwd))  ====---"; echo ; fi
-make --no-print-directory  ARCH=arm CROSS_COMPILE=$ARM_TOOLCHAIN_PRFIX SYSROOT=${sysroot} $1
+make ARCH=$arch CROSS_COMPILE=$TOOLCHAIN_PRFIX SYSROOT=${sysroot} $1
